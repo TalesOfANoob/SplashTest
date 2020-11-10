624e173a903dd02a198abe3c7314cc4c7a1c1f5e
TalesOfANoob <44920433+TalesOfANoob@users.noreply.github.com>
2020-11-10 17:57:39 +0200

    Splash Screen

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {View} from 'react-native';
 import {MainStackNavigator} from './src/navigation';
-
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
+
+  const [hideSplash,setHideSplash] = useState(false);
+
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setHideSplash(true);
+    },1200);
+  },[]);
+
+  useEffect(()=>{
+    hideSplash && SplashScreen.hide();
+  },[hideSplash]);
+
+  console.log(hideSplash);
+ 
   return (
 
         <View style={{flex: 1}}>
